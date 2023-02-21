1b6d64977b4733fa2d9df4c16315250eaaf006bc
Cabadga <121972248+Cabadga@users.noreply.github.com>
2023-02-21 14:28:04 -0800

    Lab 3 Step 3 Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 5;
+    public float xContraint = 14.3f;
+    public float zContraint = 5.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,37 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+       if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+       if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+       if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+       if (transform.position.x > xContraint)
+        {
+            transform.position = new Vector3(xContraint, transform.position.y, transform.position.z);
+        }
+       if (transform.position.x < -xContraint)
+        {
+            transform.position = new Vector3(-xContraint, transform.position.y, transform.position.z);
+        }
+       if (transform.position.z > zContraint)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zContraint);
+        }
+       if (transform.position.z < -zContraint)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zContraint);
+        }
     }
 }
