04e00380df6c19efa4b9d66e08993c8a413db779
Cabadga <121972248+Cabadga@users.noreply.github.com>
2023-02-23 21:27:06 -0800

    Lab 3 Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,37 +17,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    // Moves the player based on key input
+    void MovePlayer()
+    {
+       if (Input.GetKey(KeyCode.W)) {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
-       if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
+       if (Input.GetKey(KeyCode.S)) {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
-       if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+       if (Input.GetKey(KeyCode.A)) {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
-       if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+       if (Input.GetKey(KeyCode.D)) {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
-       if (transform.position.x > xContraint)
-        {
-            transform.position = new Vector3(xContraint, transform.position.y, transform.position.z);
+    }
+
+    // Prevent the player from leaving the screen
+    void ConstrainPlayerPosition()
+    {
+       if (transform.position.x > xContraint) {
+        transform.position = new Vector3(xContraint, transform.position.y, transform.position.z);
         }
-       if (transform.position.x < -xContraint)
-        {
-            transform.position = new Vector3(-xContraint, transform.position.y, transform.position.z);
+       if (transform.position.x < -xContraint) {
+        transform.position = new Vector3(-xContraint, transform.position.y, transform.position.z);
         }
-       if (transform.position.z > zContraint)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, zContraint);
+       if (transform.position.z > zContraint) {
+        transform.position = new Vector3(transform.position.x, transform.position.y, zContraint);
         }
-       if (transform.position.z < -zContraint)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -zContraint);
+       if (transform.position.z < -zContraint) {
+        transform.position = new Vector3(transform.position.x, transform.position.y, -zContraint);
         }
     }
 }
